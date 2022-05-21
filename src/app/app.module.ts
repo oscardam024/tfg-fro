8dfec0856cb3bdab2a3a57c5fafd2edb3df8b6a4
ogarcia <ogarcia@hiberus.com>
2022-05-21 18:35:26 +0200

    confirmado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './modules/home/home/home.component';
+import { CalendarioComponent } from './modules/calendario/calendario/calendario.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
