c55a68cfc13ce16a17d1f84100f858f283b0f0a7
ogarcia <ogarcia@hiberus.com>
2022-05-22 23:27:48 +0200

    confirmado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddprofeComponent } from './modules/addprofe/addprofe.component';
+import { AlumnosComponent } from './modules/alumnos/alumnos.component';
 import { HomeComponent } from './modules/home/home/home.component';
+import { IncidenciasComponent } from './modules/incidencias/incidencias.component';
+import { InventarioComponent } from './modules/inventario/inventario.component';
+import { PrestamosComponent } from './modules/prestamos/prestamos.component';
+import { ProfesoresComponent } from './modules/profesores/profesores.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'alumnos', component: AlumnosComponent},
+  { path: 'profesores', component: ProfesoresComponent},
+  { path: 'inventario', component: InventarioComponent},
+  { path: 'prestamos', component: PrestamosComponent},
+  { path: 'incidencias', component: IncidenciasComponent},
+  { path: 'addprofesores', component: AddprofeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './modules/home/home/home.component';
-import { CalendarioComponent } from './modules/calendario/calendario/calendario.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { FlatpickrModule } from 'angularx-flatpickr';
-
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { InventarioComponent } from './modules/inventario/inventario.component';
+import { IncidenciasComponent } from './modules/incidencias/incidencias.component';
+import { PrestamosComponent } from './modules/prestamos/prestamos.component';
+import { AlumnosComponent } from './modules/alumnos/alumnos.component';
+import { ProfesoresComponent } from './modules/profesores/profesores.component';
+import { ClasesComponent } from './modules/clases/clases.component';
+import { AddprofeComponent } from './modules/addprofe/addprofe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    InventarioComponent,
+    IncidenciasComponent,
+    PrestamosComponent,
+    AlumnosComponent,
+    ProfesoresComponent,
+    ClasesComponent,
+    AddprofeComponent,
     
     
   ],
@@ -26,6 +40,10 @@ import { FlatpickrModule } from 'angularx-flatpickr';
     FormsModule,
     NgbModalModule,
     FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
     
   ],
   providers: [],
